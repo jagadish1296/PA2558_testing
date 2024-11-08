506b4d61a854f2ef917eef3912e13d119d9d7c17
jagadish1296 <jagadish.vaddiparthi.1296@gmail.com>
2024-11-08 21:05:45 +0100

    test and func passed

--- list_string_functions.py
+++ list_string_functions.py
@@ -1,1 +1,12 @@
 def rotate_list(lst, k):
+    """
+    Rotate a list by k positions.
+
+    :param lst: The input list
+    :param k : Number of positions to rotate list (positive for right rotation negative for left)
+    :return: The rotated list
+    """ 
+    if not lst:
+        return []
+    k = k % len(lst) #handle cases where k > len(lst)
+    return lst[-k:] + lst[:-k]
